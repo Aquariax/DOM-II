901f4f3febd05b79ef8ee6b9cdf58d69757dad77
Aquariax <roberthrrs8@gmail.com>
2019-03-05 19:42:28 -0500

    added click event to bottom div

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const vin = document.querySelector('.content-pick');
+vin.addEventListener('click' ,function(eventObject){
+    console.log(`Current Target: ${eventObject.currentTarget}`);
+    eventObject.target.style.color = "red";}
+)
+
+const tim = document.querySelector('.btn');
+tim.addEventListener('mouseover' ,function(eventObject){
+    eventObject.target.size = 3;}
+)
